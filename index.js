8426dfe159c56390c467db5fcf717bc947c62bce
Levani Birchadze <levanbirchadze@gmail.com>
2023-10-20 14:15:15 +0400

    add morgan for logs and updted post

--- index.js
+++ index.js
@@ -1,7 +1,26 @@
 const express = require("express");
+const morgan = require("morgan");
 const app = express();
 
 app.use(express.json());
+
+app.use(morgan("tiny"));
+
+morgan.token("post-data", (request, response) => {
+  if (request.method === "POST") {
+    return JSON.stringify(request.body);
+  }
+  return "";
+});
+
+const postLog =
+  ":method :url :status :response-time ms - :res[content-length] :post-data";
+
+app.use(
+  morgan(postLog, {
+    skip: (request, response) => request.method !== "POST",
+  })
+);
 
 let persons = [
   {
@@ -67,14 +86,16 @@ const generateId = () => {
 app.post("/api/persons", (request, response) => {
   const body = request.body;
 
-  if (!body.name) {
+  if (!body.name || !body.number) {
     return response.status(400).json({
-      error: "name must be unique",
+      error: "Both name and number are required",
     });
   }
-  if (!body.number) {
+
+  const nameExists = persons.some((person) => person.name === body.name);
+  if (nameExists) {
     return response.status(400).json({
-      error: "number must be unique",
+      error: "Name must be unique",
     });
   }
 
