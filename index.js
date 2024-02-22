360cd25f0ebf18f50130d607058c45c222a36efe
Levani Birchadze <levanbirchadze@gmail.com>
2024-02-22 12:22:22 +0400

    recreate from 3.1 to 3.6

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const express = require("express");
 const app = express();
+app.use(express.json());
 
 let persons = [
   {
@@ -24,6 +25,12 @@ let persons = [
   },
 ];
 
+// generate random id for persons
+const generateId = () => {
+  const maxId = persons.length > 0 ? Math.max(...persons.map((n) => n.id)) : 0;
+  return maxId + 1;
+};
+
 // get all persons
 app.get("/api/persons/", (request, response) => {
   response.json(persons);
@@ -43,6 +50,41 @@ app.get("/info", (request, response) => {
   response.send(`<p>Phonebook has info for ${persons.length} people</p>
                  <br/>
                  <p>${now}</p>`);
+});
+
+// delete person
+app.delete("/api/persons/:id", (request, response) => {
+  const id = Number(request.params.id);
+  persons = persons.filter((person) => person.id !== id);
+  response.status(204).end();
+});
+
+// add person
+app.post("/api/persons", (request, response) => {
+  const body = request.body;
+
+  if (!body.name || !body.number) {
+    return response.status(400).json({
+      error: "name or number is missing",
+    });
+  }
+
+  const isNameUnique = persons.some((person) => person.name === body.name);
+  if (isNameUnique) {
+    return response.status(400).json({
+      error: "name must be unique",
+    });
+  }
+
+  const person = {
+    id: generateId(),
+    name: body.name,
+    number: body.number,
+  };
+
+  persons = persons.concat(person);
+
+  response.json(person);
 });
 
 const PORT = 3001;
