5934cc40b8e10cb641ba2f005bc90e8849ea7eac
Levani Birchadze <levanbirchadze@gmail.com>
2024-02-27 16:53:43 +0400

    update

--- index.js
+++ index.js
@@ -1,10 +1,12 @@
 const express = require("express");
 const morgan = require("morgan");
+const cors = require("cors");
 const app = express();
 
 app.use(express.json());
-
 app.use(morgan("tiny"));
+
+app.use(cors());
 
 morgan.token("post-data", (request, response) => {
   if (request.method === "POST") {
