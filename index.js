b45df42c977075479c45b105afda0b27783c618b
Levani Birchadze <levanbirchadze@gmail.com>
2024-04-09 13:08:30 +0400

    add error handlings and fix /info

--- index.js
+++ index.js
@@ -11,6 +11,16 @@ app.use(morgan("tiny"));
 app.use(cors());
 app.use(express.static("dist"));
 
+const errorHandler = (error, request, response, next) => {
+  console.error(error.message);
+
+  if (error.name === "CastError") {
+    return response.status(400).send({ error: "malformatted id" });
+  }
+
+  next(error);
+};
+
 morgan.token("post-data", (request, response) => {
   if (request.method === "POST") {
     return JSON.stringify(request.body);
@@ -42,35 +52,39 @@ app.get("/api/persons/", (request, response) => {
 
 // get single preson
 app.get("/api/persons/:id", (request, response) => {
-  Person.findById(request.params.id).then((person) => {
-    response.json(person);
-  });
+  Person.findById(request.params.id)
+    .then((person) => {
+      person ? response.json(person) : response.status(404).end();
+    })
+    .catch((error) => next(error));
 });
 
 // get amount of people and request time
 app.get("/info", (request, response) => {
-  Person.countDocuments({}, (error, count) => {
-    if (error) {
-      console.error("Error counting persons:", error);
-      response.status(500).json({ error: "Internal server error" });
-    } else {
+  Person.countDocuments({})
+    .then((count) => {
       const now = new Date();
       response.send(`<p>Phonebook has info for ${count} people</p>
-                     <br/>
-                     <p>${now}</p>`);
-    }
-  });
+                   <br/>
+                   <p>${now}</p>`);
+    })
+    .catch((error) => {
+      console.error("Error counting persons:", error);
+      response.status(500).json({ error: "Internal server error" });
+    });
 });
 
 // delete person
 app.delete("/api/persons/:id", (request, response) => {
-  Person.findByIdAndDelete(request.params.id).then((deletedPerson) => {
-    if (deletedPerson) {
-      response.status(204).end();
-    } else {
-      response.status(404).json({ error: "Person not found" });
-    }
-  });
+  Person.findByIdAndDelete(request.params.id)
+    .then((deletedPerson) => {
+      if (deletedPerson) {
+        response.status(204).end();
+      } else {
+        response.status(404).json({ error: "Person not found" });
+      }
+    })
+    .catch((error) => next(error));
 });
 
 // add person
@@ -88,10 +102,16 @@ app.post("/api/persons", (request, response) => {
     number: body.number,
   });
 
-  person.save().then((savedPerson) => {
-    response.json(savedPerson);
-  });
+  person
+    .save()
+    .then((savedPerson) => {
+      response.json(savedPerson);
+    })
+    .catch((error) => next(error));
 });
+
+app.use(errorHandler);
+
 const PORT = process.env.PORT;
 app.listen(PORT, () => {
   console.log(`server running on port ${PORT}`);
