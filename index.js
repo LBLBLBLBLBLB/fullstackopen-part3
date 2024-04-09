58ef2b1595e959aaf74a12d66258ad61fc268195
Levani Birchadze <levanbirchadze@gmail.com>
2024-04-09 18:35:54 +0400

    add eslit

--- index.js
+++ index.js
@@ -39,11 +39,6 @@ app.use(
   })
 );
 
-// // generate random id for persons
-// const generateId = () => {
-//   const maxId = persons.length > 0 ? Math.max(...persons.map((n) => n.id)) : 0;
-//   return maxId + 1;
-// };
 
 // get all persons
 app.get("/api/persons/", (request, response) => {
